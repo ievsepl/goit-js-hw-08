a42bbf2a5e9704ec9af84179b7e77ff0a2dd3623
ievsepl <107007762+ievsepl@users.noreply.github.com>
2022-10-27 20:37:40 +0300

    ch2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const newGallery = onCreateNewItem(galleryItems);
+
+gallery.insertAdjacentHTML('afterbegin', newGallery);
+gallery.addEventListener('click', onOpenPic);
+
+const largeImage = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
+
+function onCreateNewItem(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, description, original }) =>
+        `<a class="gallery__item"   href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+       alt="${description}"      
+    />
+  </a>`
+    )
+    .join('');
+}
+function onOpenPic(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  largeImage.open(event.target);
+}
