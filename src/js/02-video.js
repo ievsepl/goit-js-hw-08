ceff18081e27f76ac3e196f94eed766b53450a1f
ievsepl <107007762+ievsepl@users.noreply.github.com>
2022-10-31 19:05:56 +0200

    add hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+// const iframe = document.querySelector('iframe');
+const player = new Player('vimeo-player');
+player.on('timeupdate', throttle(onSaveTime, 1000));
+
+function onSaveTime(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
+let savedTime = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(savedTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,59 @@
+import throttle from 'lodash.throttle';
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  submit: document.querySelector('.feedback-form'),
+  inputEmail: document.querySelector('input'),
+  inputMessage: document.querySelector('textarea'),
+};
 
+// console.log(inputEmail.value);
+
+const formData = {};
+refs.form.addEventListener('input', throttle(onSaveMessage, 300));
+refs.submit.addEventListener('submit', onSubmitMessage);
+
+onSavedTextArea();
+
+function onSaveMessage(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function onSavedTextArea() {
+  const savedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (savedFormData) {
+    try {
+      refs.inputEmail.value = savedFormData.email;
+      refs.inputMessage.value = savedFormData.message;
+    } catch (error) {}
+  }
+  console.log(savedFormData);
+}
+
+function onSubmitMessage(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  if (e) {
+    console.log(formData);
+  }
+  localStorage.removeItem('feedback-form-state');
+}
+
+// const save = (key, value) => {
+//   try {
+//     const serializedState = JSON.stringify(value);
+//     localStorage.setItem(key, serializedState);
+//   } catch (error) {
+//     console.error('Set state error: ', error.message);
+//   }
+// };
+
+// const load = key => {
+//   try {
+//     const serializedState = localStorage.getItem(key);
+//     return serializedState === null ? undefined : JSON.parse(serializedState);
+//   } catch (error) {
+//     console.error('Get state error: ', error.message);
+//   }
+// };
